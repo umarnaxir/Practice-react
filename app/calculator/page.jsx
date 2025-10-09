cd884eeb5a80c2059f4f4fb8e9fbe96d51bd1d37
umarnaxir <erumarnazir@gmail.com>
2025-10-09 11:11:04 +0530

    Calculator added

---
+++ app/calculator/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CalculatorApp from "./components/CalculatorApp";
+function Home(){
+    return(
+        <div>
+            <CalculatorApp/>
+        </div>
+    );
+}
+export default Home;
